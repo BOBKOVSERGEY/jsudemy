6a76ef127cdf66d8abfff58042e98682e84f73a6
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-06-21 12:34:11 +0300

    zd

--- budgety/app.js
+++ budgety/app.js
@@ -1,29 +1,82 @@
+// BUDGET CONTROLLER
 var budgetController = (function() {
-  var x = 23;
-
-  var add = function(a) {
-    return x + a;
-  }
-
-  return {
-    publicTest: function(b) {
-      return add(b);
-    }
-  }
-})();
-
-var UIController = (function() {
   // some code
 })();
 
+// UI CONTROLLER
+var UIController = (function() {
+
+  var DOMstrings = {
+    inputType: '.add__type',
+    inputDescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn'
+  }
+  
+  return {
+    getInput: function() {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value
+      }
+    },
+    getDOMstrings: function() {
+      return DOMstrings;
+    }
+  }
+
+})();
+
+// GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
 
-  var z = budgetCtrl.publicTest(5);
+  var setupEventListenrs = function() {
+
+    var DOM = UICtrl.getDOMstrings();
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+      if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+
+  }
+
+  var ctrlAddItem = function() {
+
+    // 1. Get the field input data
+
+    var input = UICtrl.getInput();
+
+    // 2. Add the item budget controller
+
+    // 3. Add the item to the UI
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+
+  }
 
   return {
-    anotherPublic: function() {
-      console.log(z);
+    init: function() {
+      console.log('Application has started');
+      setupEventListenrs();
     }
   }
 
 })(budgetController, UIController);
+
+controller.init();
+
+
+
+
+
+
+
+
+
+
