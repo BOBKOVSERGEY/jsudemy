68482397d370e553b739c6f0c0ad6f0cd55d9afa
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-08-30 15:39:45 +0300

    delete id

--- budgety/app.js
+++ budgety/app.js
@@ -75,13 +75,25 @@ var budgetController = (function() {
       data.budget = data.totals.inc - data.totals.exp;
 
       // calculate the percenttage of income that we spent
-      data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
-
+      if (data.totals.inc > 0) {
+        data.percentage = Math.round((data.totals.exp / data.totals.inc) * 100);
+      } else {
+        data.percentage = -1;
+      }
+      
       // Expense = 100 and income 200, spent 50% = 100/200 = 0.5 * 100
 
 
     },
 
+    getBudget: function() {
+      return {
+        budget: data.budget,
+        totalInc: data.totals.inc,
+        totalExp: data.totals.exp,
+        percentage: data.percentage
+      }
+    },
 
     testing: function() {
       console.log(data);
@@ -102,6 +114,12 @@ var UIController = (function() {
       inputBtn: '.add__btn',
       incomeContainer: '.income__list',
       expensesContainer: '.expenses__list',
+      budgetLabel: '.budget__value',
+      incomeLabel: '.budget__income--value',
+      expensesLabel: '.budget__expenses--value',
+      percentageLabel: '.budget__expenses--percentage',
+      container: '.container',
+
   };
   
   //console.log(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
@@ -153,6 +171,21 @@ var UIController = (function() {
         fieldsArr[0].focus();
 
       },
+
+      displayBudget: function(obj) {
+
+        document.querySelector(DOMstrings.budgetLabel).textContent = obj.budget;
+        document.querySelector(DOMstrings.incomeLabel).textContent = obj.totalInc;
+        document.querySelector(DOMstrings.expensesLabel).textContent = obj.totalExp;
+
+        if (obj.percentage > 0) {
+          document.querySelector(DOMstrings.percentageLabel).textContent = obj.percentage + '%';
+        } else {
+          document.querySelector(DOMstrings.percentageLabel).textContent = '---';
+        }
+
+
+      },
       
       getDOMstrings: function() {
           return DOMstrings;
@@ -177,6 +210,8 @@ var controller = (function(budgetCtrl, UICtrl) {
       }
     });
 
+    document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
   };
 
   var updateBudget = function() {
@@ -184,8 +219,10 @@ var controller = (function(budgetCtrl, UICtrl) {
     budgetCtrl.calculateBudget();
 
     // 2. return the budget
+    var budget = budgetCtrl.getBudget();
 
     // 3. Display the budget on the UI
+    UICtrl.displayBudget(budget);
   }
 
   var ctrlAddItem = function() {
@@ -215,11 +252,43 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     }
 
-  }
+  };
+
+  var ctrlDeleteItem = function(event) {
+
+    var itemId, splitId, type, ID;
+    //console.log(event.target);
+    // возвращает родительский элемент
+    itemId = event.target.parentNode.parentNode.parentNode.parentNode.id;
+    
+    if (itemId) {
+
+      //inc-1
+      splitId = itemId.split('-');
+
+      type = splitId[0];
+
+      ID = splitId[1];
+
+      // 1. delete the item from the data structure
+
+      // 2. Delete the item from the UI
+
+      // 3. Update and show the new  budget
+
+    }
+
+  };
 
   return {
     init: function() {
       console.log('Application has started');
+      UICtrl.displayBudget({
+        budget: 0,
+        totalInc: 0,
+        totalExp: 0,
+        percentage: -1
+      });
       setupEventListenrs();
     }
   }
