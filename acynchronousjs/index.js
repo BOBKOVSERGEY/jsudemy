4f72e55761bc807000a238e21b2d9c1eb46d718d
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2019-01-17 16:11:22 +0300

    Settimeout

--- acynchronousjs/index.js
+++ acynchronousjs/index.js
@@ -1,4 +1,4 @@
-const second = () => {
+/*const second = () => {
   //console.log('Second');
   setTimeout(() => {
     console.log('Async');
@@ -12,3 +12,36 @@ const first = () => {
 }
 
 first();
+
+const image = document.getElementById('img').src;
+
+processLargeImage(image, () => {
+  console.log('Image process');
+})*/
+
+function getRecipe() {
+  setTimeout(() => {
+    const recipeId = [523, 883, 432, 974];
+
+    console.log(recipeId);
+
+    setTimeout(id => {
+      const recipe = {
+        title: 'Fresh tomato pasta',
+        publisher: 'Sergey'
+      };
+      console.log(`${id}: ${recipe.title}`);
+
+      setTimeout(publisher => {
+        const recipe = {
+          title: 'Italian Pizza',
+          publisher: 'Sergey'
+        };
+        console.log(recipe);
+
+      }, 1500, recipe.publisher);
+    }, 1500, recipeId[2])
+  }, 1500)
+}
+
+getRecipe();
