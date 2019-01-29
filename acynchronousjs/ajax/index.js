09b09026364ad454a361f5f7cb827ede532ba7b3
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2019-01-29 16:31:47 +0300

    Обработка json через ajax

--- acynchronousjs/ajax/index.js
+++ acynchronousjs/ajax/index.js
@@ -1,5 +1,16 @@
-fetch('https://crossorigin.me/https://www.metaweather.com/api/location/2487956/')
+fetch('data.json')
 .then(result => {
   console.log(result);
+  // возвращаем json
+  return result.json();
+})
+.then(data => {
+  console.log(data);
+  const events = data.events;
+
+  for (let i = 0; i < events.length; i++) {
+    console.log(`Место проведения ${events[i].location} дата ${events[i].date}`);
+  }
+
 })
 .catch(error => console.log(error));
